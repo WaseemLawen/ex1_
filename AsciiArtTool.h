46424a643ab567b63a8822dcca360afca0327164
waseemLawen <waseem.lawin@campus.technion.ac.il>
2022-04-28 21:06:01 +0300

    asciiart

---
+++ AsciiArtTool.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include<stdlib.h>
+
+
+#include "RLEList.h"
+#include "RLEList.c"
+
+RLEList asciiArtRead(FILE* in_stream);
+
+RLEListResult asciiArtPrint(RLEList list, FILE *out_stream);
+
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream)
